533860843972243ab497bd554e37ec0b5cb6a566
happyfeetw <xdwang1212@gmail.com>
2020-04-14 12:04:01 +0800

    init commit.

---
+++ client/src/main/java/org/apache/rocketmq/client/impl/producer/TopicPublishInfo.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.rocketmq.client.impl.producer;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.rocketmq.client.common.ThreadLocalIndex;
+import org.apache.rocketmq.common.message.MessageQueue;
+import org.apache.rocketmq.common.protocol.route.QueueData;
+import org.apache.rocketmq.common.protocol.route.TopicRouteData;
+
+public class TopicPublishInfo {
+    private boolean orderTopic = false;
+    private boolean haveTopicRouterInfo = false;
+    private List<MessageQueue> messageQueueList = new ArrayList<MessageQueue>();
+    //
+    private volatile ThreadLocalIndex sendWhichQueue = new ThreadLocalIndex();
+    private TopicRouteData topicRouteData;
+
+    public boolean isOrderTopic() {
+        return orderTopic;
+    }
+
+    public void setOrderTopic(boolean orderTopic) {
+        this.orderTopic = orderTopic;
+    }
+
+    public boolean ok() {
+        return null != this.messageQueueList && !this.messageQueueList.isEmpty();
+    }
+
+    public List<MessageQueue> getMessageQueueList() {
+        return messageQueueList;
+    }
+
+    public void setMessageQueueList(List<MessageQueue> messageQueueList) {
+        this.messageQueueList = messageQueueList;
+    }
+
+    public ThreadLocalIndex getSendWhichQueue() {
+        return sendWhichQueue;
+    }
+
+    public void setSendWhichQueue(ThreadLocalIndex sendWhichQueue) {
+        this.sendWhichQueue = sendWhichQueue;
+    }
+
+    public boolean isHaveTopicRouterInfo() {
+        return haveTopicRouterInfo;
+    }
+
+    public void setHaveTopicRouterInfo(boolean haveTopicRouterInfo) {
+        this.haveTopicRouterInfo = haveTopicRouterInfo;
+    }
+
+    /**
+     * 发送消息时选择topic下具体队列的方法。
+     * 第一次发送时调用空参的重载方法。
+     * @param lastBrokerName 上一次发送时的目标broker
+     * @return
+     */
+    public MessageQueue selectOneMessageQueue(final String lastBrokerName) {
+        if (lastBrokerName == null) {
+            return selectOneMessageQueue();
+        } else {
+            int index = this.sendWhichQueue.getAndIncrement();
+            // 若第一次发送不成功，则会产生一个lastBrokerName的值，同时触发消息重发机制
+            // 消息重发机制中，此处的循环作为重发时选择队列的方法。
+            for (int i = 0; i < this.messageQueueList.size(); i++) {
+                int pos = Math.abs(index++) % this.messageQueueList.size();
+                if (pos < 0)
+                    pos = 0;
+                MessageQueue mq = this.messageQueueList.get(pos);
+                // 判断拿到的broker是否与之前失败的broker相同
+                // 若是，则重新循环获取可用的队列
+                if (!mq.getBrokerName().equals(lastBrokerName)) {
+                    return mq;
+                }
+            }
+            return selectOneMessageQueue();
+        }
+    }
+
+    /**
+     * 第一次发送时走这个方法。
+     * 先用
+     * @return
+     */
+    public MessageQueue selectOneMessageQueue() {
+        // 线程私有的变量自增，获得队列索引
+        int index = this.sendWhichQueue.getAndIncrement();
+        // 根据队列索引值与队列总数取模，获得实际的目标队列标识
+        int pos = Math.abs(index) % this.messageQueueList.size();
+        if (pos < 0)
+            pos = 0;
+        // 根据队列标识选择要发送的队列
+        return this.messageQueueList.get(pos);
+    }
+
+    /**
+     * 根据brokerName获取该broker中可写入的队列数量
+     * @param brokerName
+     * @return
+     */
+    public int getQueueIdByBroker(final String brokerName) {
+        for (int i = 0; i < topicRouteData.getQueueDatas().size(); i++) {
+            final QueueData queueData = this.topicRouteData.getQueueDatas().get(i);
+            if (queueData.getBrokerName().equals(brokerName)) {
+                return queueData.getWriteQueueNums();
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return "TopicPublishInfo [orderTopic=" + orderTopic + ", messageQueueList=" + messageQueueList
+            + ", sendWhichQueue=" + sendWhichQueue + ", haveTopicRouterInfo=" + haveTopicRouterInfo + "]";
+    }
+
+    public TopicRouteData getTopicRouteData() {
+        return topicRouteData;
+    }
+
+    public void setTopicRouteData(final TopicRouteData topicRouteData) {
+        this.topicRouteData = topicRouteData;
+    }
+}
